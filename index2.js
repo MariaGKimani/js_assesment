fa767858d1d6e79f687b0d4740432eab2028b470
Mariagoretti <mariakimanigoreti@gmail.com>
2023-05-01 09:17:58 +0300

    add

--- index2.js
+++ index2.js
@@ -22,16 +22,10 @@ console.log(getAvailableBooks());
 // 2. Create a function getBooksByAuthor that takes 
 // an author's name as an argument and
 // returns an array of all books by that author.
-function getBooksByAuthor(bauthor){
-    const Allbooks = books.author
-        if (bauthor.author = books.author){
-            return books.values
-        }
+function getBooksByAuthor(authorN){
+    return books.filter(book => book.author === authorN);
 }
-console.log(getBooksByAuthor());
- 
-console.log(getBooksByAuthor('J.D. Salinger'));
-
+console.log(getBooksByAuthor('J.D. Salinger',));
 
 // Create a function addNewBook that takes a book object 
 // as an argument and adds it to the library, ensuring
@@ -40,39 +34,53 @@ console.log(getBooksByAuthor('J.D. Salinger'));
 
 
 function addNewBook(book){
-   x =  books.assign(books)
+  let  x = ['title','author','publicationYear','isAvailable']
+  const Properties = x.every(property => book.hasOwnProperty(property));
+  
+  if (Properties) {
+    books.push(book);
+    console.log('New book added successfully.');
+  } else {
+    console.log('Invalid book object. Please make sure it has all the required properties.');
+  }
 }
-addNewBook()
-
-
-
-// Create a function checkoutBook that takes a book title
-//  as an argument and changes the book's isAvailable property to false.
-//  If the book is not found in the library, the
-// function should return a message indicating that the book is not available.
-// function checkoutBook(booktitle){
-//     x = Object.keys(isAvailable == true)
-//     if(){
-
-//     }else{
-
-//     }
-//  }
+console.log(addNewBook({ title: 'born a crime',
+author: 'Trevoh Noah',
+publicationYear: 2000,
+isAvailable: true}));
+console.log(books);
 
 
 
 
 
-// Create a function returnBook that takes a book title as an argument and changes the
-// book's isAvailable property to true. If the book is not found in the library, the function
-// should return a message indicating that the book does not belong to the library.
-function returnBook (bookstitle){
-    this.title = bookstitle
-   if(bookstitle === bookstitle.title){
-    books.isAvailable = true
-   }
-   else{
-    console.log(`The ${this.book} does not belong to the array`)
-   }
-}
-console.log(returnBook(books))
+// // Create a function checkoutBook that takes a book title
+// //  as an argument and changes the book's isAvailable property to false.
+// //  If the book is not found in the library, the
+// // function should return a message indicating that the book is not available.
+// // function checkoutBook(booktitle){
+// //     x = Object.keys(isAvailable == true)
+// //     if(){
+
+// //     }else{
+
+// //     }
+// //  }
+
+
+
+
+
+// // Create a function returnBook that takes a book title as an argument and changes the
+// // book's isAvailable property to true. If the book is not found in the library, the function
+// // should return a message indicating that the book does not belong to the library.
+// function returnBook (bookstitle){
+//     this.title = bookstitle
+//    if(bookstitle === bookstitle.title){
+//     books.isAvailable = true
+//    }
+//    else{
+//     console.log(`The ${this.book} does not belong to the array`)
+//    }
+// }
+// console.log(returnBook(books))
